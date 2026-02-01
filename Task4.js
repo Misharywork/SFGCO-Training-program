bd77964496f827d9727db83915c710b743e2682b
Mishary <mishforwork@gmail.com>
2026-02-01 10:34:30 +0300

    addedTask4.js

---
+++ Task4.js
@@ -0,0 +1,34 @@
+const prog = [ 
+    'Code' ,
+    'Run' ,
+    'Server' ,
+    'Network',
+    'Javascript',
+    'TCP',
+    'html',
+    'debug',
+    'Website',
+     ]
+
+
+function groupLength(string) {
+
+let result = {}
+for ( let i = 0; i < string.length; i++) {
+
+    let str = string[i]
+    let length = str.length
+
+    if ( result[length] === undefined ) {
+        result[length] = []
+    }
+
+    result[length].push(str)
+
+}
+
+return result;
+
+}
+
+console.log(groupLength(prog));
