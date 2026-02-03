b5503adb0d04472caff98750900fc5d067caf9b6
Mishary <mishforwork@gmail.com>
2026-02-03 11:52:31 +0300

    File Orgnaization

--- Error/Error JavaScript2.js
+++ Error/Error JavaScript2.js
@@ -1,19 +1,3 @@
-// function divide(a, b) {
-    // if ( b === 0 ) {
-        // throw new Error("Cannot Divide by zero!");
-// }
-// return a / b;
-// }
-
-// try {
-    // console.log(divide(10, 2));
-    // console.log(divide(10, 0));
-// } catch (error) {
-    // console.log("Error!!! ,", error.message);
-// }
-
-
-
 export function checkage(age) {
     if ( age < 0) {
         throw new Error("Age Cannot Be Negative");
