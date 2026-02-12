212f4945104eedf6782f2372b0e061f92b691b8c
Mishary <mishforwork@gmail.com>
2026-02-12 09:44:36 +0300

    Small Project

---
+++ functions/SetTimeOut.js
@@ -0,0 +1,8 @@
+// setTimeout(function () {
+//     console.log('This message is displayed after a delay of 2 seconds.');
+// }, 2000);
+
+
+// setInterval(function () {
+//     console.log('This message is displayed every 3 seconds.');
+// }, 3000);
